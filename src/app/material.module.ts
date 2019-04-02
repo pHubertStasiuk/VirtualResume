96c61d120896a0e9ac6be3b56848fe58b06fef44
Hubert Stasiuk <hstasiuk@coreservices.pl>
2019-04-02 23:12:52 +0200

    Module for AngularMaterial components

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const modules = [
+];
+
+@NgModule({
+  declarations: [],
+  imports: [...modules],
+  exports: [...modules]
+})
+export class MaterialModule { }
